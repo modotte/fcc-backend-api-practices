df9c1bd9eb1581e1cbe96985f0cf547b6aae4cab
modotte <runecroon@gmail.com>
2022-07-05 13:06:11 +0800

    Remove fixme comment

--- app/Main.hs
+++ app/Main.hs
@@ -37,7 +37,6 @@ app = do
   Scotty.get "/service/filemetadata" $ do
     Scotty.file "filemetadata.html"
 
-  -- FIXME: Handle empty file!!
   Scotty.post "/api/fileanalyse" $ do
     fs <- Scotty.files
     let fi = snd $ Prelude.head fs
