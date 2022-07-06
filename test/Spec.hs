16b4687e3a3ac81d742c78097c8539004f3693c9
modotte <runecroon@gmail.com>
2022-07-06 15:50:57 +0800

    Add basic tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,31 @@
+import qualified Data.Text.Lazy as TL
+import Data.Utility as U
 import Relude
 import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+
+unitTests :: TestTree
+unitTests =
+  testGroup
+    "Unit tests"
+    [ testCase "get user-agent header value" $
+        let ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36"
+         in U.getHeader "User-Agent" [("User-Agent", ua)] @?= TL.toStrict ua,
+      testCase "get accept-language header value" $
+        let al = "en-US,en;q=0.9"
+         in U.getHeader "Accept-Language" [("Accept-Language", al)] @?= TL.toStrict al
+    ]
+
+propertyTests :: TestTree
+propertyTests =
+  testGroup
+    "Property tests"
+    []
+
+allTests :: TestTree
+allTests = testGroup "All test" [unitTests, propertyTests]
 
 main :: IO ()
 main = do
-  putStrLn "Test suite not yet implemented"
-  exitFailure
+  defaultMain allTests
